b5cbb9f3c0158e54f549a1878d33004f8e4b55a4
comtvset <comtvset@gmail.com>
2025-05-09 22:32:25 +0200

    test: add solution for simple-tests

--- src/01-simple-tests/index.test.ts
+++ src/01-simple-tests/index.test.ts
@@ -1,32 +1,81 @@
 // Uncomment the code below and write your tests
-// import { simpleCalculator, Action } from './index';
+import { simpleCalculator, Action } from './index';
 
 describe('simpleCalculator tests', () => {
   test('should add two numbers', () => {
-    // Write your test here
+    const data = {
+      a: 2,
+      b: 3,
+      action: Action.Add,
+    };
+
+    const result = simpleCalculator(data);
+    expect(result).toBe(5);
   });
 
   test('should subtract two numbers', () => {
-    // Write your test here
+    const data = {
+      a: 5,
+      b: 3,
+      action: Action.Subtract,
+    };
+
+    const result = simpleCalculator(data);
+    expect(result).toBe(2);
   });
 
   test('should multiply two numbers', () => {
-    // Write your test here
+    const data = {
+      a: 2,
+      b: 2,
+      action: Action.Multiply,
+    };
+
+    const result = simpleCalculator(data);
+    expect(result).toBe(4);
   });
 
   test('should divide two numbers', () => {
-    // Write your test here
+    const data = {
+      a: 6,
+      b: 2,
+      action: Action.Divide,
+    };
+
+    const result = simpleCalculator(data);
+    expect(result).toBe(3);
   });
 
   test('should exponentiate two numbers', () => {
-    // Write your test here
+    const data = {
+      a: 2,
+      b: 2,
+      action: Action.Exponentiate,
+    };
+
+    const result = simpleCalculator(data);
+    expect(result).toBe(4);
   });
 
   test('should return null for invalid action', () => {
-    // Write your test here
+    const data = {
+      a: 1,
+      b: 1,
+      action: 'invalid',
+    };
+
+    const result = simpleCalculator(data);
+    expect(result).toBe(null);
   });
 
   test('should return null for invalid arguments', () => {
-    // Write your test here
+    const data = {
+      a: 'a',
+      b: 'b',
+      action: Action.Add,
+    };
+
+    const result = simpleCalculator(data);
+    expect(result).toBe(null);
   });
 });
